44a03a9374036a80d25c0115c4c8b52a118583ea
Oliver Steele <steele@osteele.com>
2021-08-24 15:04:56 +0800

    Add test; refactor out unpackArgumentList

--- p5.vector-arguments/p5.vector-arguments.js
+++ p5.vector-arguments/p5.vector-arguments.js
@@ -44,50 +44,65 @@ function enableVectorArguments(p5Instance) {
   function wrap(object, propertyName, argListSpec) {
     const originalFn = object[propertyName];
     object[propertyName] = function () {
-      let i = 0, o = 0;
-      const args = new Array(arguments.length);
-      for (let arity of argListSpec) {
-        if (i >= arguments.length)
-          break;
-        let arg = arguments[i++];
-        if (arity === 3 && drawingContext instanceof CanvasRenderingContext2D) {
-          arity = 2;
-        }
-        if (arity === 2 || arity === 3) {
-          if (arg instanceof p5.Vector) {
-            args[o++] = arg.x;
-            args[o++] = arg.y;
-            if (arity == 3) {
-              args[o++] = arg.z;
-            }
-          } else if (Array.isArray(arg) && arg.length === arity) {
-            args[o++] = arg[0];
-            args[o++] = arg[1];
-            if (arity == 3) {
-              args[o++] = arg[2];
-            }
-            // Read the next n numbers from the argument list. Use
-            // `isNaN(Number(arg))` instead of `typeof arg === 'number'`, in
-            // order to preserve the underlying behavior of e.g. circle('10',
-            // '20', '50').
-          } else if (!Number.isNaN(Number(arg))) {
-            const stop = o + arity;
-            do {
-              args[o++] = arg;
-              if (o === stop) break;
-              arg = arguments[i++];
-            } while (!Number.isNaN(Number(arg)));
-            if (o < stop) break;
-          } else
-            break;
-          continue;
-        }
-        args[o++] = arg;
-      }
-      while (i < arguments.length) {
-        args[o++] = arguments[i++];
-      }
+      const args = unpackArgumentList(arguments, argListSpec);
       return originalFn.apply(this, args);
     };
+  }
+
+  function unpackArgumentList(args, argListSpec) {
+    let includesVectors = false;
+    for (let i = 0; i < args.length; i++) {
+      if (args[i] instanceof p5.Vector) {
+        includesVectors = true;
+        break;
+      }
+    }
+    if (!includesVectors) {
+      return args;
+    }
+    let i = 0, o = 0;
+    const result = new Array(args.length);
+    for (let arity of argListSpec) {
+      if (i >= args.length)
+        break;
+      let arg = args[i++];
+      if (arity === 3 && drawingContext instanceof CanvasRenderingContext2D) {
+        arity = 2;
+      }
+      if (arity === 2 || arity === 3) {
+        if (arg instanceof p5.Vector) {
+          result[o++] = arg.x;
+          result[o++] = arg.y;
+          if (arity == 3) {
+            result[o++] = arg.z;
+          }
+        } else if (Array.isArray(arg) && arg.length === arity) {
+          result[o++] = arg[0];
+          result[o++] = arg[1];
+          if (arity == 3) {
+            result[o++] = arg[2];
+          }
+          // Read the next n numbers from the argument list. Use
+          // `isNaN(Number(arg))` instead of `typeof arg === 'number'`, in
+          // order to preserve the underlying behavior of e.g. circle('10',
+          // '20', '50').
+        } else if (!Number.isNaN(Number(arg))) {
+          const stop = o + arity;
+          do {
+            result[o++] = arg;
+            if (o === stop) break;
+            arg = args[i++];
+          } while (!Number.isNaN(Number(arg)));
+          if (o < stop) break;
+        } else
+          break;
+        continue;
+      }
+      result[o++] = arg;
+    }
+    while (i < args.length) {
+      result[o++] = args[i++];
+    }
+    return result;
   }
 }
---
+++ p5.vector-arguments/tests/shapes/sketch.js
@@ -0,0 +1,38 @@
+/* This runs various interpolations of Vector and scalar arguments.
+* It should draw two columns, each with a circle above two rectangles.
+* Both circles should have the same radius. All rectangles should have
+* the same width and height.
+*/
+
+function setup() {
+  createCanvas(200, 400);
+  enableVectorArguments();
+  console.info('enable');
+
+  circles();
+  rects();
+}
+
+function circles() {
+  let pt = createVector(50, 50);
+  circle(pt, 50);
+
+  pt.add(100, 0);
+  circle(pt.x, pt.y, 50);
+}
+
+function rects() {
+  let pt = createVector(25, 100);
+  let size = createVector(50, 40);
+
+  rect(pt, size);
+
+  pt.add(100, 0);
+  rect(pt, 50, 40);
+
+  pt.add(-100, 60);
+  rect(pt, size);
+
+  pt.add(100, 0);
+  rect(pt.x, pt.y, 50, 40);
+}
