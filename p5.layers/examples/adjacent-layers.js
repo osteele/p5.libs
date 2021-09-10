ced393321b52e412b1b4e603fa127a8ba728bf60
osteele <steele@osteele.com>
2021-09-10 12:12:54 +0000

    Deploying to gh-pages from @ osteele/p5.libs@8753a7e5ff9391d1f8225aa5441e8e8d727b9465 🚀

---
+++ p5.layers/examples/adjacent-layers.js
@@ -0,0 +1,30 @@
+// Description: Call `enterLayer()` with two different keys, in order to create
+// two Graphics that are drawn side by side.
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  beginLayer('left', width / 2, height);
+  colorMode(RGB, height, width, height);
+  background(200);
+  for (let y = 0; y < height; y++) {
+    stroke((y + frameCount) % height, width - mouseX, height - mouseY);
+    line(0, y, width, height - y)
+  }
+  fill('white');
+  text("RGB Mode", 10, 15);
+  endLayer();
+
+  beginLayer('right', width / 2, height);
+  colorMode(HSB, height, width, height);
+  background(200);
+  for (let y = 0; y < height; y++) {
+    stroke((y + frameCount) % height, width - mouseX, height - mouseY);
+    line(0, y, width, height - y)
+  }
+  fill('white');
+  text("HSB Mode", 10, 15);
+  endLayer(width / 2, 0);
+}
