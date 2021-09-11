1384dddb80e867d324f8325cf95419f03328a6d2
Oliver Steele <steele@osteele.com>
2021-09-11 19:13:10 +0800

    Fix an issue in the transformed (published) version

--- p5.vector-arguments/CHANGELOG.md
+++ p5.vector-arguments/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [1.0.1]
+
+- Fix an issue in the transformed (published) version
+
 ## [1.0.0]
 
 - Initial public release
--- p5.vector-arguments/p5.vector-arguments.js
+++ p5.vector-arguments/p5.vector-arguments.js
@@ -58,56 +58,51 @@
   /** Modify the p5.js functions to accept vectors. */
 
   function enableVectorArguments() {
-    // Each key is a list of argument types. 2 indicates a 2D vector, 3 is a 2D or 3D vector
-    // depending on the canvas type. A n-D vector can be saturated by the first n components
-    // of a p5.Vector, by an Array with length n, or by n arguments from the argument list.
-    const functionArgTypes = {
-      'createCanvas': [2],
-      'createGraphics': [2],
-      'createImage': [2],
-      'text': [0, 2, 2],
-      'image': [0, 2],
-      'arc': [2, 2],
-      'circle': [2],
-      'ellipse': [2, 2, 25],
-      'line': [3, 3],
-      'point': [3],
-      'quad': [3, 3, 3],
-      'rect': [2, 2],
-      'square': [2],
-      'triangle': [2, 2, 2],
-      'bezier': [3, 3, 3, 3],
-      'bezierVertex': [3, 3, 3],
-      'curve': [3, 3, 3, 3],
-      'curveVertex': [3],
-      'vertex': [3],
-      'quadraticVertex': [3, 3],
-      'plane': [2],
-      'box': [3],
-      'ellipsoid': [3]
-    };
-
     for (const [key, argListSpec] of Object.entries(functionArgTypes)) {
-      this instanceof p5 && wrap(this, key, argListSpec);
-      wrap(window, key, argListSpec);
+      this instanceof p5 && replaceFunction(this, key, argListSpec);
+      replaceFunction(window, key, argListSpec);
     }
 
-    wrap(p5.Element.prototype, 'position', [2]);
-  } // call the original function with an argument list in which Vectors and
-  // Arrays have been replaced by their components.
+    replaceFunction(p5.Element.prototype, 'position', [2]);
+  }
+  /* Each key is a list of argument types. 2 indicates a 2D vector, 3 is a 2D or 3D vector
+   * depending on the canvas type. A n-D vector can be saturated by the first n components
+   * of a p5.Vector, by an Array with length n, or by n arguments from the argument list.
+   */
 
 
-  function wrap(object, propertyName, argListSpec) {
+  const functionArgTypes = {
+    'createCanvas': [2],
+    'createGraphics': [2],
+    'createImage': [2],
+    'text': [0, 2, 2],
+    'image': [0, 2],
+    'arc': [2, 2],
+    'circle': [2],
+    'ellipse': [2, 2, 25],
+    'line': [3, 3],
+    'point': [3],
+    'quad': [3, 3, 3],
+    'rect': [2, 2],
+    'square': [2],
+    'triangle': [2, 2, 2],
+    'bezier': [3, 3, 3, 3],
+    'bezierVertex': [3, 3, 3],
+    'curve': [3, 3, 3, 3],
+    'curveVertex': [3],
+    'vertex': [3],
+    'quadraticVertex': [3, 3],
+    'plane': [2],
+    'box': [3],
+    'ellipsoid': [3]
+  };
+  /* Call the original function with an argument list in which Vectors and
+   * Arrays have been replaced by their components.
+  */
+
+  function replaceFunction(object, propertyName, argListSpec) {
     const originalFn = object[propertyName];
-
-    function newFn() {
-      const args = unpackArgumentList(arguments, argListSpec);
-      return originalFn.apply(this, args);
-    }
-
-    newFn.displayName = originalFn.displayName;
-    newFn.name = originalFn.name;
-    newFn.length = originalFn.length;
+    const newFn = wrap(originalFn, argListSpec);
     const prop = Object.getOwnPropertyDescriptor(object, propertyName);
 
     if (prop) {
@@ -119,6 +114,17 @@
     } else {
       object[propertyName] = newFn;
     }
+  }
+  /* Create a new function that expands the argument list to include vectors
+   * and then calls the original function.
+   */
+
+
+  function wrap(fn, argListSpec) {
+    return function () {
+      const args = unpackArgumentList(arguments, argListSpec);
+      return fn.apply(this, args);
+    };
   }
 
   function unpackArgumentList(args, argListSpec) {
