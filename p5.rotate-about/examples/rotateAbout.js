a03f480deafad25e2f0e6077a57aa82fd7f4c3b3
Oliver Steele <steele@osteele.com>
2021-10-19 17:49:25 +0800

    Deploying to gh-pages:publish

---
+++ p5.rotate-about/examples/rotateAbout.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  clear();
+
+  const angle = frameCount;
+  rotateAbout(angle, width / 2, height / 2);
+
+  line(width / 2, 0, width / 2, height);
+  line(0, height / 2, width, height / 2);
+
+  rect(width / 2 - 50, height / 2 - 50, 75, 75);
+  rect(width / 2, height / 2, 100, 100);
+
+  textSize(20);
+  text(`angle = ${angle.toFixed(0)}°`, width / 2, 20);
+}
